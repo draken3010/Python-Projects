92ac91a1be14e132b95cb2eb9e7faf4758e04da1
Harsh Patel <pharsh8188@gmail.com>
2022-09-17 10:42:09 +0530

    Revert "dice_stimulator - project 2"
    This reverts commit 1b6a36f2fd065588eff6af78deb2a926ed387eee.

--- Dice_stimulator.py
+++
@@ -1,9 +0,0 @@
-'''Welcome to rolling dice stimualtor - Lost your dice? Dont worry, just run this program and your problem will be solved'''
-
-import random
-
-def roll_dice():
-    choice_list = [1,2,3,4,5,6]
-    return random.choice(choice_list)
-
-print(roll_dice())
